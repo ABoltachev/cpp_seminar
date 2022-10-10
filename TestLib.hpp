53f68d1c6a2ea84270656f379b47993d621f8f76
Aleksandr Boltachev <boltahcev.alex@gmail.com>
2022-10-10 08:19:23 +0300

    Init commit

---
+++ TestLib.hpp
@@ -0,0 +1,36 @@
+// TODO
+// TODO
+#define TESTLIB_H
+
+// TODO
+// TODO
+
+    // TODO
+    // TODO
+    Data max(Data a, Data b);
+
+    struct Point {
+        int x;
+        int y;
+    };
+
+    /*TODO*/  /*TODO*/ Point* coordsVector_impl(
+        /*TODO*//*TODO*/
+    );
+
+    Point coordsVector(/*TODO*//*TODO*/);
+
+    // TODO
+    // TODO
+    void print(Args... args);
+
+    // TODO
+    // TODO
+    void print(const Head &head, Tail... tail);
+
+    // TODO
+    // TODO
+
+
+// TODO
+// TODO
