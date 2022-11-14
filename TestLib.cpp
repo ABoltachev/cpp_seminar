2c4a2912f672a768d1c0269c2435ac68e52b5808
Aleksandr Boltachev <boltahcev.alex@gmail.com>
2022-11-14 09:00:05 +0300

    After

--- TestLib.cpp
+++ TestLib.cpp
@@ -1,5 +1,48 @@
 #include "TestLib.hpp"
 
+#include <cassert>
+
 namespace TestLib {
-    ;
+    AddressBook::AddressBook(const Employee &data) {
+        m_head = new Node(Employee(data.getUid(), data.getUsername(), data.getName(), data.getSalaty()));
+        m_size = 1;
+    }
+
+    AddressBook::AddressBook(size_t size) {
+        for (size_t i = 0; i < size; i++) {
+            addEmployee();
+        }
+    }
+
+    void AddressBook::addEmployee() {
+        m_size++;
+        if (!m_head) {
+            m_head = new Node;
+            return;
+        }
+        Node *cur_node = m_head;
+        while (cur_node->next) {
+            cur_node = cur_node->next;
+        }
+        cur_node->next = new Node;
+    }
+
+    void AddressBook::setEmployeeData(size_t idx, uint32_t uid, const std::string &username,
+                                      const std::string &name, float salary) {
+        Employee data = at(idx);
+        data.m_uid = uid;
+        data.m_username = username;
+        data.m_name = name;
+        data.m_salary = salary;
+    }
+
+    Employee& AddressBook::at(size_t idx) {
+        assert(idx >= m_size);
+
+        Node *cur_node = m_head;
+        for (size_t i = 0; i < idx; i++) {
+            cur_node = cur_node->next;
+        }
+        return cur_node->m_data;
+    }
 }
--- TestLib.hpp
+++ TestLib.hpp
@@ -6,39 +6,65 @@
 #include <string>
 
 namespace TestLib {
+    class Employee;
+
+    class AddressBook {
+    private:
+        class Node {
+        public:
+            Employee m_data;
+            Node *next = nullptr;
+
+            Node() = default;
+            Node(const Employee &data) : m_data(data) {}
+        };
+        Node *m_head;
+        size_t m_size;
+    public:
+        AddressBook(const Employee &data);
+
+        AddressBook(size_t size);
+
+        Employee& at(size_t idx);
+
+        void setEmployeeData(size_t idx, uint32_t uid, const std::string &username,
+                             const std::string &name, float salary);
+
+        void addEmployee();
+
+        void addEmployee(uint32_t uid, const std::string &username,
+                         const std::string &name, float salary);
+
+        ~AddressBook();
+
+        friend class Logger;
+    };
+
     class Employee {
     private:
         uint32_t m_uid;
         std::string m_username;
         std::string m_name;
         float m_salary;
-
     public:
-        uint32_t getUid(); //
-        std::string getUsername(); //
-        std::string getName(); //
-        float getSalaty(); //
+        Employee() = default;
+        Employee(uint32_t uid, const std::string &username, const std::string &name, float salary);
+        uint32_t getUid() const;
+        std::string getUsername() const;
+        std::string getName() const;
+        float getSalaty() const;
+        friend void AddressBook::setEmployeeData(size_t idx, uint32_t uid, const std::string &username,
+                                                 const std::string &name, float salary);
     };
 
-    class AddressBook {
-    private:
-        Node *m_head;
-        size_t m_size;
-    public:
-        AddressBook(size_t size);
-
-        Employee& at(size_t idx);
-
-        void setEmployeeData();
-    };
 
     class Logger {
     private:
-        std::ofstream;
+        std::ofstream file;
     public:
         Logger(const std::string &log_file);
 
-        void print();
+        void print(const AddressBook &address_book);
     };
 }
 
